f57fc6579c302b78b072304ea5a3b36a9556794c
James Leung <jleung2101@gmail.com>
2024-04-10 23:07:50 -0400

    updated file output to show work and added test case

--- pagerank.py
+++ pagerank.py
@@ -40,25 +40,41 @@ def calculate_pagerank(urls, links, convergence_threshold, max_iterations, dampi
     for source, target in links:
         outbound_links_count[source] += 1
     count = 0
-    while count < max_iterations:
-        for url in urls:
-            # for each predecessor (j of IN(v_i)), add score / its outgoing links to sum  (ingoing means coming to curr url, so target is curr url)
-            predecessor_sum_score = sum(prev_pagerank[source] / outbound_links_count[source] for source, target in links if target == url and outbound_links_count[source] != 0)
+    with open("pagerank_work.output", "w") as file:
+        file.write("0.25 for all 0th iterations\n\n")
+        
+        while count < max_iterations:
+            file.write(f"Iteration {count + 1}:\n")
+            for url in urls:
+                # for each predecessor (j of IN(v_i)), add score / its outgoing links to sum  (ingoing means coming to curr url, so target is curr url)
+                predecessor_sum_score = 0
+                i = 0
+                file.write(f"{url}: ")
+                file.write("[")
+                for source, target in links:
+                    if target == url and outbound_links_count[source] != 0:
+                        predecessor_sum_score += prev_pagerank[source] / outbound_links_count[source]
+                    if i == 0:
+                        file.write(f"({prev_pagerank[source]} / {outbound_links_count[source]}) ")
+                    else:
+                        file.write(f"+ ({prev_pagerank[source]} / {outbound_links_count[source]}) ")
+                    i += 1
+                # pagerank for this url is (1-d) / N + d * predecessor sum score
+                pagerank[url] = round((1 - damping_factor) / N + damping_factor * predecessor_sum_score, 3)
+                    
+                file.write(f"] * {damping_factor} + (1 - {damping_factor}) / {N} = {round(pagerank[url], 3)}\n")
+                    
+            # need to go through all urls to see if curr score - prev score < threshold (using all func that takes in iterable)
+            converged = all(abs(pagerank[url] - prev_pagerank[url]) < convergence_threshold for url in urls)
+            if converged:
+                # normalize scores according to sum of scores
+                total_sum = sum([score for score in pagerank.values()])
+                for curr_url, score in pagerank.items():
+                    pagerank[curr_url] /= total_sum
+                break
 
-            # pagerank for this url is (1-d) / N + d * predecessor sum score
-            pagerank[url] = (1 - damping_factor) / N + damping_factor * predecessor_sum_score
-
-        # need to go through all urls to see if curr score - prev score < threshold (using all func that takes in iterable)
-        converged = all(abs(pagerank[url] - prev_pagerank[url]) < convergence_threshold for url in urls)
-        if converged:
-            # normalize scores according to sum of scores
-            total_sum = sum([score for score in pagerank.values()])
-            for curr_url, score in pagerank.items():
-                pagerank[curr_url] /= total_sum
-            break
-
-        prev_pagerank = pagerank.copy()
-        count += 1
+            prev_pagerank = pagerank.copy()
+            count += 1
     print(f"Rounds to converge: {count}")
     return pagerank
 
@@ -66,7 +82,7 @@ def write_pagerank(filename, pagerank):
     """Write sorted PageRank scores to a file."""
     with open(filename, 'w') as f:
         for url, score in sorted(pagerank.items(), key=lambda x: x[1], reverse=True):
-            f.write(f"{url} {score}\n")
+            f.write(f"{url} {round(score, 3)}\n")
 
 if __name__ == "__main__":
     if len(sys.argv) != 5:
