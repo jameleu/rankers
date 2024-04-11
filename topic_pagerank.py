98b93f835b070f70056e7ef89cedce72bab6c729
James Leung <jleung2101@gmail.com>
2024-04-10 23:08:53 -0400

    updated output to show work for topic pagerank

--- topic_pagerank.py
+++ topic_pagerank.py
@@ -36,45 +36,66 @@ def calculate_pagerank(urls, links, biases, query_rel, url_topics, convergence_t
     for source, target in links:
         outbound_links_count[source] += 1
     topics_to_word_scores = defaultdict(dict)
-    for curr_bias in query_rel.keys(): 
-        print("**********************", curr_bias)
-        topics_to_word_scores[curr_bias] = {url: 0.25 for url in urls}
-        pagerank = topics_to_word_scores[curr_bias]
-        prev_pagerank = pagerank.copy()
-        count = 0
-        while count < max_iterations:
-            for url in urls:
-                bias_factor = 0
-                if curr_bias in url_topics[url]:
-                    bias_factor = float(1)/biases[curr_bias]
-                    print(f"b{bias_factor}")
-                print("_______________", url)
-                predecessor_sum_score = round(sum((prev_pagerank[source] / outbound_links_count[source]) for source, target in links if target == url and outbound_links_count[source] != 0), 4)
-                print(predecessor_sum_score)
-                pagerank[url] = round(((1 - damping_factor) * bias_factor + damping_factor * predecessor_sum_score), 3)
-                print(pagerank[url])
-            # need to go through all urls to see if curr score - prev score < threshold (using all func that takes in iterable)
-            converged = all(abs(pagerank[url] - prev_pagerank[url]) < convergence_threshold for url in urls)
-            if converged:
-                # normalize scores according to sum of scores
-                total_sum = sum([score for score in pagerank.values()])
-                for curr_url, score in pagerank.items():
-                    pagerank[curr_url] /= total_sum
-                break
-
+    with open("topic_pagerank_work.output", "w") as file:
+        for curr_bias in query_rel.keys(): 
+            topics_to_word_scores[curr_bias] = {url: 0.25 for url in urls}
+            pagerank = topics_to_word_scores[curr_bias]
             prev_pagerank = pagerank.copy()
-            count += 1
+            count = 0
+            file.write(f"Current topic: {curr_bias}\n")
+            while count < max_iterations:
+                file.write(f"Iteration {count + 1}\n")
+                for url in urls:
+                    file.write(f"{url}: ")
+                    bias_factor = 0
+                    if curr_bias in url_topics[url]:
+                        bias_factor = float(1)/biases[curr_bias]
+                    predecessor_sum_score = 0
+                    file.write("[")
+                    i = 0    
+                    for source, target in links:
+                        if target == url and outbound_links_count[source] != 0:
+                            predecessor_sum_score += prev_pagerank[source] / outbound_links_count[source]
+                            if i == 0:
+                                file.write(f"({prev_pagerank[source]} / {outbound_links_count[source]}) ")
+                            else:
+                                file.write(f"+ ({prev_pagerank[source]} / {outbound_links_count[source]}) ")
+                            i += 1
+                    predecessor_sum_score = round(predecessor_sum_score, 4)
+                    pagerank[url] = round(((1 - damping_factor) * bias_factor + damping_factor * predecessor_sum_score), 3)
+                    file.write(f"] * {damping_factor} + (1 - {damping_factor}) * {bias_factor} = {round(pagerank[url], 3)}\n")
+                # need to go through all urls to see if curr score - prev score < threshold (using all func that takes in iterable)
+                file.write("\n")
+                converged = all(abs(pagerank[url] - prev_pagerank[url]) < convergence_threshold for url in urls)
+                if converged:
+                    # normalize scores according to sum of scores
+                    total_sum = sum([score for score in pagerank.values()])
+                    for curr_url, score in pagerank.items():
+                        pagerank[curr_url] /= total_sum
+                    break
+                prev_pagerank = pagerank.copy()
+                count += 1
+            file.write("\n")
         print(f"Rounds to converge: {count}")
     return topics_to_word_scores
 
 def calculate_final_ranks(query_rel, pagerank_set, biases, urls):
     final = defaultdict(float)
-    for url in urls:
-        curr_score = 0
-        for topic, rel in query_rel.items():
-            print(f"{topic}, {url} : {pagerank_set[topic][url]}")
-            curr_score += pagerank_set[topic][url] * rel
-        final[url] = round(curr_score, 4)
+    with open("topic_pagerank_work.output", "a") as file:
+        file.write("Final Query Relevance Rank Calculations:\n")
+        for url in urls:
+            curr_score = 0
+            i = 0
+            file.write(f"{url}: ")
+            for topic, rel in query_rel.items():
+                curr_score += pagerank_set[topic][url] * rel
+                if i == 0:
+                    file.write(f"{pagerank_set[topic][url]} * {rel}")
+                else:
+                    file.write(f" + {pagerank_set[topic][url]} * {rel}")
+                i += 1    
+            final[url] = round(curr_score, 4)
+            file.write(f" = {final[url]}\n")
     return final
         
 def write_pagerank(filename, pagerank):
